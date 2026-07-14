82048be9f00be51d1ef5d9da1320b786469e3e5a
jotavgalves <jvgacontato@gmail.com>
2026-07-14 20:27:01 -0300

    chore: block css patches and important rules

---
+++ app/stylelint.config.js
@@ -0,0 +1,11 @@
+export default {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "declaration-no-important": true,
+    "no-duplicate-selectors": true,
+    "selector-max-id": 0,
+    "selector-max-specificity": "0,4,0",
+    "selector-class-pattern": "^(?!.*(?:fix|patch|temp|final|new))[a-z][a-z0-9]*(?:-[a-z0-9]+)*$",
+    "color-no-invalid-hex": true
+  }
+};
